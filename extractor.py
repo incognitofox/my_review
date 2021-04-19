1c283f0189060a18e12f9feeada7554c6036bc1a
incognitofox <wwill2011@gmail.com>
2021-04-19 01:50:31 -0500

    csv generation

--- extractor.py
+++ extractor.py
@@ -5,5 +5,16 @@ Created on Mon Apr 19 00:17:50 2021
 @author: Will
 """
 
-def get_questions(evaluation):
+import csv
+
+def create_csv(questions_with_responses):
+    with open('test.csv', 'w+') as f:
+        for question in questions_with_responses.keys():
+            row = question
+            responses = questions_with_responses[question]
+            for response in responses:
+                row += ' ' + str(response)
+            row += '\n'
+            f.write(row)
+    return
     
