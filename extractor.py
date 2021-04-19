5fbb72ad19f7262e291c10b0d5268c91d567e861
incognitofox <wwill2011@gmail.com>
2021-04-19 11:42:47 -0500

    basic web framework

--- extractor.py
+++ extractor.py
@@ -7,8 +7,8 @@ Created on Mon Apr 19 00:17:50 2021
 
 import csv
 
-def create_csv(questions_with_responses):
-    with open('test.csv', 'w+') as f:
+def create_csv(questions_with_responses, filename):
+    with open(filename, 'w+') as f:
         for question in questions_with_responses.keys():
             row = question
             responses = questions_with_responses[question]
@@ -16,5 +16,15 @@ def create_csv(questions_with_responses):
                 row += ' ' + str(response)
             row += '\n'
             f.write(row)
-    return
-    
+    return filename
+
+def create_html(questions_with_responses, filename):
+    html = "<table><tr><td>Questions</td></tr>"
+    for question in questions_with_responses.keys():
+        row = "<tr><td>" + str(question) + "</td>"
+        responses = questions_with_responses[question]
+        for response in responses:
+            row += "<td>" + str(response) + "</td>"
+        row += "</tr>"
+        html += row
+    return html
---
+++ index.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 19 10:47:15 2021
+
+@author: Will
+"""
+
+from flask import Flask, render_template, request, jsonify
+import os, io, sys
+import pandas as pd
+import myReviewScraper
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/scrape")
+def scrape():
+    print('scraping')
+    myReviewScraper.test()
+    return jsonify(result=0)
+
+if __name__ == '__main__':
+    app.run(threaded=True, port=8080)
