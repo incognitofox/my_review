efe20c844a486542380e2bce67ef2b35d03f2638
incognitofox <wwill2011@gmail.com>
2021-04-20 00:27:51 -0500

    does the thing; multiple times

--- myReviewScraper.py
+++ myReviewScraper.py
@@ -7,54 +7,77 @@ from selenium.webdriver.common.keys import Keys
 import re
 import time
 import extractor
+import pickle
+#import asyncio
 
 
 def test():
-    driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+    #driver = webdriver.Chrome('/usr/local/bin/chromedriver')
     # for Windows machines, put chromedriver.exe in directory of project:
-    # driver = webdriver.Chrome('./chromedriver.exe')
+    driver = webdriver.Chrome('./chromedriver.exe')
     scroll(driver)
     URL = get_url(driver)
     flag = True
     while(flag):
         links = driver.find_elements_by_xpath("//td[@class='title']/a")
+        evals = {}
         for link in links:
-            link = link.get_attribute('href')
+            #print(driver.get_cookies())
+            #pickle.dump(driver.get_cookies() , open("cookies.pkl","wb+"))
+            try:
+                link = link.get_attribute('href')
+            except:
+                continue
             driver.execute_script("window.open('');")
             driver.switch_to.window(driver.window_handles[1])
             driver.get(link)
             source = driver.page_source
-            finished, html = scrape(source)
+            title, finished, questions = scrape(source)
+            #time.sleep(2)
             if finished:
-        		# close tab
+                evals[title] = questions
+                # close tab
                 driver.close()
-        		# switch back to main tab
+                # switch back to main tab
                 driver.switch_to.window(driver.window_handles[0])
+                #driver.close()
                 scroll(driver)
                 flag = False
-                return html
-    return "empty"
+    print(evals) 
+    return evals
 
 def get_url(driver):
-	#URL = 'https://www.google.com'
-	URL = 'https://evaluations.uchicago.edu/?CourseDepartment=CMSC&CourseNumber=16100'
-	driver.get(URL)
-	driver.maximize_window()
-	return URL
+    #URL = 'https://www.google.com'
+    URL = 'https://evaluations.uchicago.edu/?CourseDepartment=CMSC&CourseNumber=16100'
+    '''
+    try:
+        cookies = pickle.load(open("cookies.pkl", "rb"))
+        for cookie in cookies:
+            #print(cookie)
+            #if 'expiry' in cookies and cookies['expiry'] > time.time():
+            print(time.time())
+            driver.add_cookie(cookie)
+    except:
+        print("cookies expired")
+    '''
+    driver.get(URL)
+    
+    driver.maximize_window()
+    return URL
 
 def scroll(driver):
-	# scroll to end of page
-	html = driver.find_element_by_tag_name("html")
-	html.send_keys(Keys.END)
-	time.sleep(0.5)
-	driver.page_source
+    # scroll to end of page
+    html = driver.find_element_by_tag_name("html")
+    html.send_keys(Keys.END)
+    time.sleep(0.5)
+    driver.page_source
 
 def scrape(source):
     ctx = ssl.create_default_context()
     ctx.check_hostname = False
     ctx.verify_mode = ssl.CERT_NONE
     soup = BeautifulSoup(source, 'html.parser')
-    
+    title = soup.find("title").text.strip()
     questions_with_responses = {}
     questions = soup.find_all(class_="report-block")
     for question in questions:
@@ -65,7 +88,7 @@ def scrape(source):
                 responses = tag.find_all("td", "TabularBody_LeftColumn")
                 responses_text = [response.text.strip() for response in responses]
                 questions_with_responses[question_text] = responses_text
-    print(questions_with_responses)
-    extractor.create_csv(questions_with_responses, 'test.csv')
-    html = extractor.create_html(questions_with_responses, 'test.csv')
-    return (len(questions_with_responses) > 0, html)
+    print(title)
+    #extractor.create_csv(questions_with_responses, 'test.csv')
+    #html = extractor.create_html(questions_with_responses, 'test.csv')
+    return (title, len(questions_with_responses) > 0, questions_with_responses)
